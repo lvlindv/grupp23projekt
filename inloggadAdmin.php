bb5b5f2858275a047fd6a1161c7a532cb2050af3
rebwen <38286470+rebwen@users.noreply.github.com>
2018-05-10 16:42:34 +0200

    Skapat inloggadadmin.php
    en sida där admin väljer om den vill registrera, ta bort eller ändra info om en studyhelper. hit kommer admin när den loggat in (ej kopplat ännu)

---
+++ inloggadAdmin.php
@@ -0,0 +1,13 @@
+
+<html>
+
+
+  <body>
+    <h1> Adminsida </h1>
+    <p> Lägga till/Ta bort eller Redigera info för studyhelper... </p>
+
+      <button type="submit" >Registrera en ny StudyHelper</button>
+      <button type="submit" >Ta bort StudyHelper</button>
+      <button type="submit" >Redigera info för en StudyHelper</button>
+  </body>
+</html>
