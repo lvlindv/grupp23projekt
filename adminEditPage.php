ca13807fc6457a07bc834c9b8273037ee2b15350
Linda <lvlindvp@gmail.com>
2018-05-23 14:46:16 +0200

    Edit page

--- adminEditPage.php
+++ adminEditPage.php
@@ -3,3 +3,21 @@
 echo "Connection worked"
 
  ?>
+
+<!DOCTYPE html>
+<html lang="sv" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Edit page</title>
+  </head>
+
+    <input type="hidden" name="coachId" value="<?php echo $coachId; ?>">
+
+    <input type="text" name="name" value="<?php echo $name; ?>">
+    <input type="text" name="email" value="<?php echo $email; ?>">
+    <input type="text" name="password" value="<?php echo $password; ?>">
+    <input type="text" name="description" value="<?php echo $description; ?>">
+    <input type="text" name="phoneNr" value="<?php echo $phoneNr; ?>">
+
+  </body>
+</html>
