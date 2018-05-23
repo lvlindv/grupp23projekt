3510fc7bdbb6b1cd6b38313fd3804760537e7b32
Emelie <emelie.loman@hotmail.com>
2018-05-23 18:34:23 +0200

    CssJusteringar

--- startpage.php
+++ startpage.php
@@ -11,38 +11,47 @@ include "db_connect.php";
 <!DOCTYPE HTML>
 <html lang="sv" dir="ltr">
   <head>
-    <script src="functions.js"></script>
     <meta charset="utf-8">
     <title>Startsida</title>
+    <script src="functions.js"></script>
+    <link rel="stylesheet" href="startpage.css">
+
   </head>
 
   <body>
+        <p class="rubrik">STUDIEHJÄLPEN </p>
+        <div class="loginbox">
+        <img src="img/gubbe.png" class="gubbe">
 
-
+        <h2>Logga in här</h2>
+            </br>
 
 <form name="loginForm" action="loginUser.php" method="post" >
 
-  <label for="uname"><b>Epost</b></label>
-  <input type="text" placeholder="Ange din E-post" name="email" >
+  <label for="uname"><b>E-post</b></label>
+  <input type="text" placeholder="Ange din e-post" name="email" >
 
   <label for="psw"><b>Lösenord</b></label>
-  <input type="password" placeholder="Ange Lösenord" name="psw" >
+  <input type="password" placeholder="Ange ditt lösenord" name="psw" >
+  <br>
+  <br>
 <!--Knapp som kopplar användaren till loginUser.php -->
   <button name="btnLogin" type="submit" value="Button" >Logga in</button>
-</br>
-<!-- Referens för glömt lösenord -->
-  <a href="glomtlosenord">Glömt ditt lösenord?</a>
-</br>
 
-</form>
+
+  </form>
 
 <!--Kopplar användaren till registreraFormStudent.php sidan. -->
-    <form name="btnRegister" action="registerFormStudent.php" method="post" >
-      <button name="btnRegister" type="submit" value="Button">Registrera</button>
-    </form>
+  <form name="btnRegister" action="registerFormStudent.php" method="post" >
+    <button name="btnRegister" type="submit" value="Button">Registrera</button>
+  </form>
 
 <!--Knapp som kopplar användaren till ett form? för att bli studycoach. (EJ I FUNKTION) -->
-<button name="btnSC" type="submit" value="Button" >Bli Studycoach</button>
+<button name="btnSC" type="submit" value="Button">Bli Studycoach</button>
+<!-- Referens för glömt lösenord -->
+  <br>
+  <br>
+  <a href="glomtlosenord">Glömt ditt lösenord?</a>
 
   </body>
 </html>
