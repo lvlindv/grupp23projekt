233ca15ea3f1ae5d6ffa9d4e1f6346847046eb82
Linda <lvlindvp@gmail.com>
2018-05-24 16:29:49 +0200

    Kommentarer
    admindeletepage, adminstartpage, booking, coachstartpage1

--- booking.php
+++ booking.php
@@ -1,32 +1,41 @@
 <?php
 
-// Start the session
+// Startar sessionen
 session_start();
 
-//Koppling till databas
+// Koppling till databasen
 include "db_connect.php";
 
-//Koppling till fil med queries
+// Koppling till fil med queries
 include "queries.php";
 
+// Tilldelar variblerna(selectedDay, selectedSubject, studentId) värdet av sessionsvariabler.
 $selectedDay = $_SESSION['selectedDay'];
 $selectedSubject = $_SESSION['selectedSubject'];
-$currentCoachId = $_POST['coachId'];
 $currentStudentId = $_SESSION['studentId'];
+// Sätter coachId till en variabel.
+$currentCoachId = $_POST['coachId'];
 
+// Lägger till en bokning i databasen, med hjälp av funktionen addBooking från queries.php
+// Placerar in sessionsvariablerna och variabeln som parametrar.
 if ($connection ->query(addBooking($selectedDay, $selectedSubject, $currentStudentId, $currentCoachId)))
 {
+  // Visar att en ny bokning lyckats
   echo "Du har lagt till en ny bokning!";
+  // Länk till studentStartpage.php
   echo '<a href="studentStartpage.php">Tillbaka till startsidan.</a>';
 }
 else
 {
+  // Visar att förfrågan ej gick igenom och printar ut SQL-satsen.
   echo "Något gick fel.". $sql. "<br>". $connection->error;
+  // Länk till studentStartpage.php
   echo '<a href="studentStartpage.php">Försök igen.</a>';
 }
+// Stänger databaskopplingen
 $connection->close();
 
-//Skickar vidare till startsida
+// Skickar vidare till startsida
 header("Location: studentStartpage.php");
 
 ?>
--- coachStartpage.php
+++ coachStartpage.php
@@ -8,49 +8,52 @@
   include "queries.php";
   //Fil som skapar dropdown listor och tabeller baserat på data från db
   include "htmlgenerator.php";
-
 ?>
+
 <!DOCTYPE html>
 <html lang="sv" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Startsida StudieCoach</title>
+    <!--Kopplar till js-fil-->
     <script src="functions.js"></script>
   </head>
-
-<body>
-  <h1> <?php echo "Välkommen ".$_SESSION["name"]."!"; ?> </h1><!--Rubrik med studiecoachens email-->
-
-  <h2> Dina bokningar </h2><!--underrubrik-->
-  <section class="myBookings"><!--ruta med bokningar-->
+    <body>
+      <!-- Rubrik med inloggade studiecoachens namn-->
+      <h1> <?php echo "Välkommen ".$_SESSION["name"]."!"; ?> </h1>
+      <!--underrubrik-->
+      <h2> Dina bokningar </h2>
+      <!-- Ruta med bokningar-->
+      <section class="myBookings">
+        <?php
+          // Lagrar resultat från query som hämtar studiecoachens bokningar och matar ut varje bokning
+          $resultBookings = $connection->query($queryCoachBookings);
+          while ($row = $resultBookings->fetch_assoc())
+          {
+            // Skriver ut bokningens dag, ämne, coachens namn och kontaktuppgifter
+            // samt studentens namn och kontaktuppgifter
+            include "showBookings.php";
+          }
+        ?>
+      </section>
+      <!--underrubrik-->
+      <h2> Lägg till tillgängliga tider </h2>
+      <form action="addAvailability.php" method="POST">
+        <!--Label för dropdown med dagar-->
+        <label for="dayDropdown"><b>Välj dag</b></label>
+          <select name="dayName">
+            <option value="Måndag">Måndag</option>
+            <option value="Tisdag">Tisdag</option>
+            <option value="Onsdag">Onsdag</option>
+            <option value="Torsdag">Torsdag</option>
+            <option value="Fredag">Fredag</option>
+          </select>
+        <!--Knapp för att lägga till tillgänglighet-->
+        <input type="submit" value="Lägg till" name="btnAdd"/>
+      </form>
     <?php
-      //Lagrar resultat från query som hämtar studiecoachens bokningar och matar ut varje bokning
-      $resultBookings = $connection->query($queryCoachBookings);
-      while ($row = $resultBookings->fetch_assoc())
-      {
-        //Skriver ut bokningens dag, ämne, coachens namn och kontaktuppgifter
-        //samt studentens namn och kontaktuppgifter
-        include "showBookings.php";
-      }
-    ?>
-  </section>
-
-  <h2> Lägg till tillgängliga tider </h2><!--underrubrik-->
-  <form action="addAvailability.php" method="POST">
-    <label for="dayDropdown"><b>Välj dag</b></label><!--Label för dropdown med dagar-->
-    <select name="dayName">
-      <option value="Måndag">Måndag</option>
-      <option value="Tisdag">Tisdag</option>
-      <option value="Onsdag">Onsdag</option>
-      <option value="Torsdag">Torsdag</option>
-      <option value="Fredag">Fredag</option>
-    </select>
-
-    <input type="submit" value="Lägg till" name="btnAdd"/><!--Knapp för att lägga till tillgänglighet-->
-  </form>
-    <?php
-    //Länk för att logga ut användare
+    // Länk för att logga ut användare
     echo '<a href="logoutUser.php">Logga ut</a>';
      ?>
-</body>
+  </body>
 </html>
