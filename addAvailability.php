598108675bfd1ee5aff97e411014eba603fcb014
louisewiljander <louise@wiljander.com>
2018-05-24 16:40:25 +0200

    Tagit bort onödig fil, ändrat filnamn och påbörjat jobb med hantering av dubletter

--- addAvailability.php
+++ addAvailability.php
@@ -1,11 +1,11 @@
 <?php
-  // Start the session
+  // Startar sessionen
   session_start();
 
-  //Koppling till databas
+  // Koppling till databas
   include 'db_connect.php';
 
-  //Koppling till fil med queries
+  // Koppling till fil med queries
   include "queries.php";
 
   if(isset($_POST['btnAdd']))
@@ -14,11 +14,12 @@
     $_SESSION['selectedDay'] = $_POST['dayName'];
   }
 
-  //Lagrar värdena av sessionvariablerna i nya varibler som ska sättas in i query
+  // Lagrar värdena av sessionvariablerna i nya varibler som ska sättas in i query
   $selectedDay = $_SESSION['selectedDay'];
   $coachId = $_SESSION['coachId'];
 
-  //Query för att lägga till tillgänglig studiecoach i databasen
+
+  // Query för att lägga till tillgänglig studiecoach i databasen
   if ($connection->query(addAvailability($selectedDay, $coachId))) {
     echo "Du har nu angett att du är tillgänglig på ".$_SESSION['selectedDay'].".";
     echo '<a href="coachStartpage.php">Tillbaka till din sida.</a>';
--- readme.md
+++
@@ -1,3 +0,0 @@
-# Studiehjälpen
-
-Detta är github:en för studiehjälpen. Världens bästa hemsida!
