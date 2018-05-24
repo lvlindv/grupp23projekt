8ec33983d6a1fc6dad1d1487e58ad3e87bfd385e
Linda <lvlindvp@gmail.com>
2018-05-24 10:31:29 +0200

    adminpage

--- adminDeletePage.php
+++ adminDeletePage.php
@@ -1,3 +1,10 @@
 <?php
 include 'db_connect.php';
  ?>
+
+<?php
+
+$coachId = $_POST['coachId'];
+echo $coachId;
+
+ ?>
--- adminStartpage.php
+++ adminStartpage.php
@@ -12,81 +12,70 @@ include "db_connect.php"
     <script src="functions.js"></script>
     <meta charset="utf-8">
     <title>Startsida</title>
-    </head>
-    <h1> <?php echo "Välkommen ".$_SESSION["name"]."!"; ?>  </h1>
-    <h2> Registrera en ny studiecoach </h2>
+  </head>
       <body>
+        <h1> <?php echo "Välkommen ".$_SESSION["email"]."!"; ?>  </h1>
+        <h2> Registrera en ny studiecoach </h2>
         <form name="regHelper" action="registerToDBCoach.php" method="post" onsubmit="return validateStudycoach()" >
 
-      <label for="name"><b>Fullständigt namn</b></label>
-      <input type="text" placeholder="Ange fullständigt namn på StudyCoachen" name="name" >
+          <label for="name"><b>Fullständigt namn</b></label>
+          <input type="text" placeholder="Ange fullständigt namn på StudyCoachen" name="name">
 
-      <label for="email"><b>E-post</b></label>
-      <input type="text" placeholder="Ange epost" name="email" >
+          <label for="email"><b>E-post</b></label>
+          <input type="text" placeholder="Ange epost" name="email" >
 
-      <label for="psw"><b>Lösenord</b></label>
-      <input type="password" placeholder="Ange lösenord" name="psw" >
+          <label for="psw"><b>Lösenord</b></label>
+          <input type="password" placeholder="Ange lösenord" name="psw" >
 
-      <label for="description"><b>Beskrivning</b></label>
-      <input type="text" placeholder="Ange en beskrivning av StudyCoachen" name="description" >
+          <label for="description"><b>Beskrivning</b></label>
+          <input type="text" placeholder="Ange en beskrivning av StudyCoachen" name="description" >
 
-      <label for="phoneNr"><b>Mobilnummer</b></label>
-      <input type="text" placeholder="Ange mobilnummer" name="phoneNr" >
+          <label for="phoneNr"><b>Mobilnummer</b></label>
+          <input type="text" placeholder="Ange mobilnummer" name="phoneNr" >
 
-      <input type="submit" value="Registrera">
-        <script src="functions.js"></script>
+          <input type="submit" value="Registrera">
 
-        </form>
+       </form>
 
-    <h2> Ta bort eller Redigera info för studiecoach </h2>
+        <?php
 
-<?php
+        $sql = "SELECT * FROM StudyCoach ORDER BY coachId DESC";
+        $result = mysqli_query($connection, $sql);
 
-$sql = "SELECT * FROM StudyCoach ORDER BY coachId DESC";
-$result = mysqli_query($connection, $sql);
+        ?>
+        <h2> Ta bort eller Redigera info för studiecoach </h2>
 
- ?>
+         <table boarder="0" cellpadding="10" cellspacing="1" width="700" class="tblListForm">
+           <tr class="listheader"><th>Namn</th><th>E-post</th><th>Lösenord</th><th>Beskrivning</th><th>Telefonummer</th><th>Alternativ</th></tr>
 
+            <?php
+             //Matar ut resultatet från queryn ovan
+             while ($row = $result->fetch_assoc())
+             {
+            ?>
 
- <table boarder="0" cellpadding="10" cellspacing="1" width="700" class="tblListForm">
-   <tr class="listheader"><th>Namn</th><th>E-post</th><th>Lösenord</th><th>Beskrivning</th><th>Telefonummer</th><th>Alternativ</th>
-   </tr>
+            <form action="adminDeletePage.php" method="post">
+              <tr>
+                <td style="visibility:hidden" name="coachId"> <?php echo $row["coachId"]; ?></td>
+                <td><?php echo $row["name"]; ?></td>
+                <td><?php echo $row["email"]; ?></td>
+                <td><?php echo $row["password"]; ?></td>
+                <td><?php echo $row["description"]; ?></td>
+                <td><?php echo $row["phoneNr"]; ?></td>
+                <td> <input type="submit" value="Ta Bort" name="btndel"></td>
+              </tr>
+            </form>
 
-   <?php
-
-     //Matar ut resultatet från queryn ovan
-     while ($row = $result->fetch_assoc())
-     {
-    // $i=0;
-       // while($row = mysqli_fetch_array($result)) {
-       // if($i%2==0)
-       // $classname="evenRow";
-       // else
-       // $classname="oddRow";
-    ?>
-     <td><?php echo $row["name"]; ?></td>
-     <td><?php echo $row["email"]; ?></td>
-     <td><?php echo $row["password"]; ?></td>
-     <td><?php echo $row["description"]; ?></td>
-     <td><?php echo $row["phoneNr"]; ?></td>
-
-     <td><form action="adminDeletePage.php" method="post">
-       <input type="submit" value="Ta Bort" name="btndel">
-     </form></td>
+            <?php
+             $i++;
+             }
+            ?>
 
 
-     </tr>
-
- <?php
- $i++;
- }
- ?>
-
-
- </table>
- <?php
-  //Länk för att logga ut användare
-   echo '<a href="logoutUser.php">Logga ut</a>';
-  ?>
-  </body>
+          </table>
+         <?php
+          //Länk för att logga ut användare
+           echo '<a href="logoutUser.php">Logga ut</a>';
+          ?>
+      </body>
 </html>
