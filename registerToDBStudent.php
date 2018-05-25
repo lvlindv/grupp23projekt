2ed703f707ae082629c2c22bd668c9b141550389
Linda <lvlindvp@gmail.com>
2018-05-25 16:36:05 +0200

    Kommentarer

--- registerToDBStudent.php
+++ registerToDBStudent.php
@@ -12,13 +12,14 @@ $email = $_POST['email'];
 $password = $_POST['psw'];
 $phoneNr = $_POST['mnr'];
 
-
+// Kollar om email inte finns i databasen
 if (!$connection->query(checkEmail($email)))
 {
   echo "Tyvärr, emailadressen finns redan registrerad!";
 }
 else
 {
+  // Lägger in studenten i databasen
   $connection->query(addStudent($name, $email, $password, $phoneNr));
   echo "Användare sparad!";
   echo '<a href="startpage.php">Tillbaka till startsidan.</a>';
