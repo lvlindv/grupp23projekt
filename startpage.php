4ef3b2c8fec49c3f43cedbd5a96893a89f91ba83
louisewiljander <louise@wiljander.com>
2018-05-25 22:57:10 +0200

    Css

--- startpage.php
+++ startpage.php
@@ -22,33 +22,36 @@ include "db_connect.php";
       <h1>STUDIEHJÄLPEN </h1>
 
       <div class="loginBox">
-
-      <!--<img src="img/gubbe.png" class="loginImg">-->
+        <div class="divLoginImg">
+          <img src="img/gubbe.png" class="loginImg">
+        </div>
       <div class="loginTitle">
         <h2>LOGGA IN</h2>
       </div>
 
       <form name="loginForm" action="loginUser.php" method="post" onsubmit="validateLogin()">
         <div class="loginForm">
-          <label for="uname"><b>E-post</b></label>
-          <input type="text" placeholder="Ange din e-post..." name="email">
-          <label for="psw"><b>Lösenord</b></label>
-          <input type="password" placeholder="Ange ditt lösenord..." name="psw">
+          <label for="uname" class="loginLabel"><b>E-post</b></label>
+          <input type="text" class="loginInput" placeholder="Ange din e-post..." name="email">
+          <label for="psw" class="loginLabel"><b>Lösenord</b></label>
+          <input type="password" class="loginInput" placeholder="Ange ditt lösenord..." name="psw">
         </div>
-        <!--Knapp som kopplar användaren till loginUser.php -->
-        <button name="btnLogin" class="btnLogin" type="submit" value="Button" >Logga in</button>
+          <!--Knapp som kopplar användaren till loginUser.php -->
+          <button name="btnLogin" class="btnLogin" type="submit" value="Button" >Logga in</button>
       </form>
-
-      <!--Kopplar användaren till registreraFormStudent.php sidan. -->
-      <form name="btnRegister" action="registerFormStudent.php" method="post" >
-        <button name="btnRegister" type="submit" class="btnRegister" value="Button">Registrera</button>
-      </form>
+        <!--Kopplar användaren till registreraFormStudent.php sidan. -->
+        <form name="btnRegister" action="registerFormStudent.php" method="post" >
+          <button name="btnRegister" type="submit" class="btnRegister" value="Button">Registrera</button>
+        </form>
 
       <!--Knapp som kopplar användaren till ett form? för att bli studycoach. (EJ I FUNKTION) -->
       <button name="btnStudyCoach" type="submit" class="btnSignup" value="Button">Bli studiecoach</button>
 
-      <!-- Referens för glömt lösenord -->
-      <a href="glomtlosenord" class="forgot">Glömt ditt lösenord?</a>
+      <div class="divForgottenPassword">
+        <!-- Referens för glömt lösenord -->
+        <a href="glomtlosenord" class="linkForgottenPassword">Glömt ditt lösenord?</a>
+      </div>
+
 
     </div>
   </body>
