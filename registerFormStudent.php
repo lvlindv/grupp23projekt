3ff3cb387e14e32299c0b2fe3873e45db31665f4
Linda <lvlindvp@gmail.com>
2018-05-25 23:29:37 +0200

    Css

--- registerFormStudent.php
+++ registerFormStudent.php
@@ -10,59 +10,43 @@
     <!--Koppling till javascript-fil-->
     <script src="functions.js"></script>
     <!-- Koppling till css-fil -->
-    <link rel="stylesheet" href="registerPage.css">
+    <link rel="stylesheet" href="main.css">
     <title>Registrering</title>
-    </head>
+  </head>
 
-      <body>
-
-      <p class="rubrik">STUDIEHJÄLPEN </p>
+   <body>
+     <h1>STUDIEHJÄLPEN </h1>
       <div class="loginbox">
-      <img src="img/gubbe.png" class="gubbe">
+
+      <h2>REGISTRERA DIG</h2>
+
       <!--Formulär för användarregistrering-->
       <form name="regForm" action="registerToDBStudent.php" method="post" onsubmit="return validateReg()" >
+       <div class="loginForm">
+          <label for="namn"><b>Ditt fullständiga namn</b></label>
+          <input type="text" placeholder="Skriv in ditt namn..." name="namn" >
 
-      <!--Titel och brödtext-->
-      <h1>REGISTRERA DIG</h1>
-      <!-- <p>Fyll i formuläret för att skapa en användare.</p> Behöver vi denna? -->
-      </br>
-      </br>
-      </br>
-      <!--Label och inputbox för namn-->
-      <div class="name">
-      <label for="namn"><b>Ditt fullständiga namn</b></label>
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Skriv in din email..." name="email" >
+
+          <label for="mnr"><b>Mobilnummer</b></label>
+          <input type="text" placeholder="Skriv in ditt mobilnummer..." name="mnr" >
+
+          <label for="psw"><b>Lösenord</b></label>
+          <input type="password" placeholder="Välj ett lösenord..." name="psw" >
       </div>
-      <input type="text" placeholder="Skriv in ditt namn..." name="namn" >
-
-      <!--Label och inputbox för e-post-->
-      <div class="email">
-      <label for="email"><b>Email</b></label>
-      </div>
-      <input type="text" placeholder="Skriv in din email..." name="email" >
-
-      <!--Label och inputbox för mobilnummer-->
-      <div class="mobnr">
-      <label for="mnr"><b>Mobilnummer</b></label>
-      </div>
-      <input type="text" placeholder="Skriv in ditt mobilnummer..." name="mnr" >
-
-      <!--Label och inputbox för lösenord-->
-      <div class="passw">
-      <label for="psw"><b>Lösenord</b></label>
-      </div>
-      <input type="password" placeholder="Välj ett lösenord..." name="psw" >
-
       <!--Knapp för registrering som skickar vidare användaren till mina sidor-->
-      <button type="submit" class="regbtn">Registrera dig</button>
+      <button type="submit" class="btnRegister">Registrera dig</button>
 
       <!--Knapp för att avbryta registrering OBS! LÄGG TILL KOPPLING TILLBAKA TILL STARTSIDA-->
-      <button type="button" class="canclebtn">Avbryt</button>
+      <button type="button" class="btnSignup">Avbryt</button>
 
       <div class="godkann">
-      <b><p>Godkänn våra <a href="#">användarvillkor</a></b>
-      <input type="checkbox" name="Godkänn"</p>
+      <p><b>Godkänn våra <a href="#">användarvillkor</a></b></p>
+      <input type="checkbox" name="Godkänn">
       </div>
       </div>
+
     </form>
   </body>
 </html>
