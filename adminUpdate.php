38a65495df87c83d99c85f94c3539b9e76657c91
Emelie <emelie.loman@hotmail.com>
2018-05-28 16:48:09 +0200

    cssErrorPage

--- adminUpdate.php
+++ adminUpdate.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!-- Koppling till javascript-fil -->
+    <script src="functions.js"></script>
+
+    <!-- Koppling till css-fil -->
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="startpage.css">
+    <link rel="stylesheet" href="popUp.css">
+
+    <title>Ett fel uppstod</title>
+    </head>
+
+    <body>
+
+    <div class="popUp">
+
 <?php
 // startar sessionen på sidan
 session_start();
@@ -23,17 +42,23 @@ $phnr = $_POST['phoneNr'];
 if ($connection->query(updateStudyCoach($name, $email, $psw, $des, $coachId, $phnr)))
 {
   // Visar att en ny studiecoach har lagts till i databasen
-  echo "Du har redigerat studiecoachen!";
+  echo '<a class="userSaved">Studiecoach redigerad!</a>';
   // Länk tillbaka till adminstartpage.php
-  echo '<a href="adminStartpage.php">Tillbaka till startsidan.</a>';
+  echo '<a href="adminStartpage.php" class="buttonBack">Tillbaka till startsidan</a>';
 }
+
+
 else
 {
   // Visar att SQL-satsen inte lyckades läggas till
-  echo "Något gick fel.". $sql. "<br>". $connection->error;
+  echo "Något gick fel...". $sql. "<br>". $connection->error;
   // Länk tillbaka till adminstartpage
   echo '<a href="adminStartpage.php">Försök igen.</a>';
 }
 // avslutar
 $connection->close();
  ?>
+
+ </div>
+ </body>
+ </html>
