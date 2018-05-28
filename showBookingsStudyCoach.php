b10012df33cba56987784e7fec2176e898070b4a
louisewiljander <louise@wiljander.com>
2018-05-28 18:47:52 +0200

    fixat bokningar css

--- showBookingsStudyCoach.php
+++ showBookingsStudyCoach.php
@@ -2,35 +2,41 @@
 
   <div class="bookingDay">
     <?php
+    echo "Dag: ";
     echo $row["day"];
     ?>
   </div>
 
   <div class="bookingSubject">
     <?php
+    echo "Ämne: ";
     echo $row["subject"];
     ?>
   </div>
 
+  <div class="studentInfo">
+    <?php
+    echo "Student: ";
+     ?>
+  <div class="studentName">
+    <?php
+    echo $row["studentName"];
+    ?>
+  </div>
 
-<div class="studentName">
-  <?php
-  echo $row["studentName"];
-  ?>
-</div>
+  <div class="studentNr">
+    <?php
+    echo $row["studentNr"];
+    ?>
+  </div>
 
-<div class="studentNr">
-  <?php
-  echo $row["studentNr"];
-  ?>
-</div>
+  <div class="studentEmail">
+    <?php
+    echo $row["studentEmail"];
 
-<div class="studentEmail">
-  <?php
-  echo $row["studentEmail"];
-
-  echo "<br />";
-  ?>
+    echo "<br />";
+    ?>
+  </div>
 </div>
 
 </div>
