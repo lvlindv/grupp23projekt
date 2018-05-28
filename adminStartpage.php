67bd94793fda5eae90ba160f0fa84d10bb192830
Linda <lvlindvp@gmail.com>
2018-05-28 23:48:03 +0200

    Kommentering

--- adminStartpage.php
+++ adminStartpage.php
@@ -25,10 +25,10 @@ loggedInAsAdmin();
     <script src="functions.js"></script>
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="admin.css">
-        <link rel="stylesheet" href="userStartPage.css">
+    <link rel="stylesheet" href="userStartPage.css">
   </head>
       <body>
-        <!-- Länk för att logga ut användare -->
+        <!-- Knapp för att logga ut användare -->
         <div class="logout">
           <button onclick="location.href='logoutUser.php';">Logga ut</button>
         </div>
@@ -37,7 +37,7 @@ loggedInAsAdmin();
 
         <div class="formRegisterCoach">
         <div class="boxTitle">
-          <h2> Registrera en ny studiecoach </h2>
+        <h2> Registrera en ny studiecoach </h2>
         </div>
         <!-- Ett formulär för att lägga till nya studiecoacher-->
         <form name="regHelper" action="registerToDBCoach.php" method="post" onsubmit="return validateStudyCoach()">
@@ -60,42 +60,39 @@ loggedInAsAdmin();
           <div class="subjects">
           <label for="description" class="labelSubjects"><b>Ämnen</b></label>
           <p class="dropdownInfo">*Håll in ctrl eller ⌘ för att välja flera ämnen</p>
-            <?php
-            // Lista över valbara ämnen för en studiecoach att hjälpa till med
-            // Queryn hämtar ämnen från databasen
-            $result_options = $connection->query(showSubjects());
-            $select_name = "subject";
+          <?php
+          // Lista över valbara ämnen för en studiecoach att hjälpa till med
+          // Queryn hämtar ämnen från databasen
+          $result_options = $connection->query(showSubjects());
+          $select_name = "subject";
 
-              // Om tabellen har fler än 0 rader exekveras koden
-              if ($result_options->num_rows > 0)
+            // Om tabellen har fler än 0 rader exekveras koden
+            if ($result_options->num_rows > 0)
+            {
+              // Skapar dropdown-lista med multiple choice
+              echo "<select multiple='multiple' name=".$select_name."[] >";
+              // Hämtar alla rader från resultatet av query
+              while ($row = $result_options->fetch_row())
               {
-                //Skapar dropdown-lista med multiple choice
-                echo "<select multiple='multiple' name=".$select_name."[] >";
-                // Hämtar alla rader från resultatet av query
-                while ($row = $result_options->fetch_row())
-                {
-                  // Matar ut options baserat på datan från tabellen
-                  $val = $row[0];
-                  echo utf8_encode("<option value=\"$val\">$val</option>\n");
-                }
-                echo "</select>";
+                // Matar ut options baserat på datan från tabellen
+                $val = $row[0];
+                echo utf8_encode("<option value=\"$val\">$val</option>\n");
               }
-            ?>
+              echo "</select>";
+            }
+          ?>
           </div>
           <button name="btnReg" class="btnregister" type="submit" value="Button">Registrera</button>
-
-       </form>
-
+        </form>
         </div>
         <?php
         // Plockar ut alla studiecoacher ur databasen
         $result = mysqli_query($connection, showStudyCoaches());
         ?>
-
         <h2> Ta bort eller redigera info för studiecoach </h2>
         <!-- En tabell där alla studiecoacher ska placeras in-->
-         <table class="studyCoachTable">
-           <tr class="listheader">
+        <table class="studyCoachTable">
+          <tr class="listheader">
              <th>Namn</th>
              <th>E-post</th>
              <th>Lösenord</th>
@@ -103,37 +100,26 @@ loggedInAsAdmin();
              <th>Telefonummer</th>
              <th>Alternativ</th>
            </tr>
-
             <?php
              // Matar ut resultatet från queryn ovan
              while ($row = $result->fetch_assoc())
              {
             ?>
             <!-- Placerar in värdena från databasen i rätt kolumn-->
-              <tr>
-                <!-- Skickar med valt coachId till adminDeletePage.php-->
-                <!-- Väljer hidden som type så att det inte visas på sidan-->
+            <tr>
+              <td><?php echo $row["name"]; ?></td>
+              <td><?php echo $row["email"]; ?></td>
+              <td><?php echo $row["password"]; ?></td>
+              <td><?php echo $row["description"]; ?></td>
+              <td><?php echo $row["phoneNr"]; ?></td>
+                <!-- Skickar coachId via URL med en GET-variabel-->
 
-                <td><?php echo $row["name"]; ?></td>
-                <td><?php echo $row["email"]; ?></td>
-                <td><?php echo $row["password"]; ?></td>
-                <td><?php echo $row["description"]; ?></td>
-                <td><?php echo $row["phoneNr"]; ?></td>
-                <!-- När inputen är "klickad" skickas coachId som ska med i formet -->
-
-
-
-                <!-- coahID skickas med url:n när användaren klickar på länken-->
-
-                <td><button class="btnDelete" onclick="location.href='adminDeletePage.php?id=<?php echo $row["coachId"]; ?>';">Ta bort</button>
-                <button class="btnEdit" onclick="location.href='adminEditPage.php?id=<?php echo $row["coachId"]; ?>';">Redigera</button></td>
-
-              </tr>
-
+              <td><button class="btnDelete" onclick="location.href='adminDeletePage.php?id=<?php echo $row["coachId"]; ?>';">Ta bort</button>
+              <button class="btnEdit" onclick="location.href='adminEditPage.php?id=<?php echo $row["coachId"]; ?>';">Redigera</button></td>
+            </tr>
             <?php
              }
             ?>
-
-          </table>
-      </body>
+        </table>
+    </body>
 </html>
