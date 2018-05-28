c194d854717e2bdd9d85bf62315df2db3e54e34c
rebwen <38286470+rebwen@users.noreply.github.com>
2018-05-29 00:09:25 +0200

    indentering

--- registerToDBStudent.php
+++ registerToDBStudent.php
@@ -13,42 +13,38 @@
     <title>Registrering</title>
     </head>
 
-    <body>
-
+  <body>
+  <!--klass för sidan som användaren kommer till från registerFormStudent-->
     <div class="popUp">
 
 
-<?php
-// Start the session
-session_start();
+      <?php
+      // Start the session
+      session_start();
 
-//Koppling till databas
-include 'db_connect.php';
+      //Koppling till databas
+      include 'db_connect.php';
+      include 'queries.php';
 
-include 'queries.php';
+      $name = $_POST['namn'];
+      $email = $_POST['email'];
+      $password = $_POST['psw'];
+      $phoneNr = $_POST['mnr'];
 
-$name = $_POST['namn'];
-$email = $_POST['email'];
-$password = $_POST['psw'];
-$phoneNr = $_POST['mnr'];
-
-// Kollar om email inte finns i databasen
-if (!$connection->query(checkEmail($email)))
-{
-  echo "Tyvärr, emailadressen finns redan registrerad!";
-}
-else
-{
-  // Lägger in studenten i databasen
-  $connection->query(addStudent($name, $email, $password, $phoneNr));
-  echo '<a class="userSaved">Användaren sparad!</a>';
-  echo '<a href="startpage.php" class="buttonBack">Tillbaka till startsidan</a>';
-
-
-
-}
-$connection->close();
-?>
+      // Kollar om email inte finns i databasen
+      if (!$connection->query(checkEmail($email)))
+      {
+      echo "Tyvärr, emailadressen finns redan registrerad!";
+      }
+      else
+      {
+      // Lägger in studenten i databasen
+      $connection->query(addStudent($name, $email, $password, $phoneNr));
+      echo '<a class="userSaved">Användaren sparad!</a>';
+      echo '<a href="startpage.php" class="buttonBack">Tillbaka till startsidan</a>';
+      }
+      $connection->close();
+      ?>
 
     </div>
   </body>
